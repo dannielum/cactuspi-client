501ecd9a4a0eace93370aedb910d2d9f54a45cf0
dannielum <dannielum@gmail.com>
2021-08-12 00:25:36 -0400

    subscribe to sqs queue url

--- services/sqs.js
+++ services/sqs.js
@@ -2,6 +2,7 @@ const AWS = require('aws-sdk');
 
 module.exports = class SQSService {
   constructor(config) {
+    this.queue = config.queue;
     this.queueUrl = config.topic;
 
     AWS.config.update({ region: config.region });
@@ -10,52 +11,57 @@ module.exports = class SQSService {
   }
 
   subscribe(sendMessage, sendCommand) {
-    const params = {
-      AttributeNames: ['userMetadata'],
-      MaxNumberOfMessages: 10,
-      MessageAttributeNames: ['All'],
-      QueueUrl: this.queueUrl,
-      VisibilityTimeout: 20,
-      WaitTimeSeconds: 0,
-    };
-
-    this.sqs.receiveMessage(params, (err, data) => {
+    this.sqs.getQueueUrl({ QueueName: this.queue }, (err, queueData) => {
       if (err) {
-        console.log('SQS - error message', err);
-      } else if (data.Messages && data.Messages.length > 0) {
-        console.log('SQS - message', data.Messages[0]);
-
-        let metadata = {};
-        try {
-          metadata = JSON.parse(
-            data.Messages[0].MessageAttributes.metadata.StringValue
-          );
-        } catch (err) {
-          console.log('SQS - error parsing metadata', err);
-          return;
-        }
-
-        console.log('SQS metadata', metadata);
-        const { command } = metadata;
-
-        if (command) {
-          sendCommand(command);
-        } else {
-          sendMessage({
-            message: data.Messages[0].Body,
-            userMetadata: metadata,
-          });
-        }
-
-        const deleteParams = {
-          QueueUrl: this.queueUrl,
-          ReceiptHandle: data.Messages[0].ReceiptHandle,
+        console.log('SQS - error subscribe to queue url', err);
+      } else {
+        const params = {
+          AttributeNames: ['userMetadata'],
+          MaxNumberOfMessages: 10,
+          MessageAttributeNames: ['All'],
+          QueueUrl: queueData.QueueUrl,
+          VisibilityTimeout: 20,
+          WaitTimeSeconds: 0,
         };
-        this.sqs.deleteMessage(deleteParams, function (err) {
+
+        this.sqs.receiveMessage(params, (err, data) => {
           if (err) {
-            console.log('SQS - error delete message', err);
-          } else {
-            console.log('SQS - success delete message', data);
+            console.log('SQS - error message', err);
+          } else if (data.Messages && data.Messages.length > 0) {
+            console.log('SQS - message', data.Messages[0]);
+
+            let metadata = {};
+            try {
+              metadata = JSON.parse(
+                data.Messages[0].MessageAttributes.metadata.StringValue
+              );
+            } catch (err) {
+              console.log('SQS - error parsing metadata', err);
+              return;
+            }
+
+            const { command } = metadata;
+
+            if (command) {
+              sendCommand(command);
+            } else {
+              sendMessage({
+                message: data.Messages[0].Body,
+                userMetadata: metadata,
+              });
+            }
+
+            const deleteParams = {
+              QueueUrl: this.queueUrl,
+              ReceiptHandle: data.Messages[0].ReceiptHandle,
+            };
+            this.sqs.deleteMessage(deleteParams, function (err) {
+              if (err) {
+                console.log('SQS - error delete message', err);
+              } else {
+                console.log('SQS - success delete message', data);
+              }
+            });
           }
         });
       }
