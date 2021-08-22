142ec0bef4e5c58922ab271394baef24aa1580b6
dannielum <dannielum@gmail.com>
2021-08-22 16:55:05 -0400

    implement long polling for sqs messages

--- services/sqs.js
+++ services/sqs.js
@@ -1,70 +1,47 @@
-const AWS = require('aws-sdk');
+const { Consumer } = require('sqs-consumer');
 
 module.exports = class SQSService {
   constructor(config) {
-    this.queue = config.queue;
     this.queueUrl = config.topic;
-
-    AWS.config.update({ region: config.region });
-
-    this.sqs = new AWS.SQS({ apiVersion: '2012-11-05' });
   }
 
   subscribe(sendMessage, sendCommand) {
-    this.sqs.getQueueUrl({ QueueName: this.queue }, (err, queueData) => {
-      if (err) {
-        console.log('SQS - error subscribe to queue url', err);
-      } else {
-        const params = {
-          AttributeNames: ['userMetadata'],
-          MaxNumberOfMessages: 10,
-          MessageAttributeNames: ['All'],
-          QueueUrl: queueData.QueueUrl,
-          VisibilityTimeout: 20,
-          WaitTimeSeconds: 0,
-        };
+    const app = Consumer.create({
+      queueUrl: this.queueUrl,
+      attributeNames: ['userMetadata'],
+      messageAttributeNames: ['All'],
+      handleMessage: (message) => {
+        console.log('SQS - message', message);
 
-        this.sqs.receiveMessage(params, (err, data) => {
-          if (err) {
-            console.log('SQS - error message', err);
-          } else if (data.Messages && data.Messages.length > 0) {
-            console.log('SQS - message', data.Messages[0]);
+        let metadata = {};
+        try {
+          metadata = JSON.parse(message.MessageAttributes.metadata.StringValue);
+        } catch (err) {
+          console.log('SQS - error parsing metadata', err);
+          return;
+        }
 
-            let metadata = {};
-            try {
-              metadata = JSON.parse(
-                data.Messages[0].MessageAttributes.metadata.StringValue
-              );
-            } catch (err) {
-              console.log('SQS - error parsing metadata', err);
-              return;
-            }
+        const { command } = metadata;
 
-            const { command } = metadata;
-
-            if (command) {
-              sendCommand(command);
-            } else {
-              sendMessage({
-                message: data.Messages[0].Body,
-                userMetadata: metadata,
-              });
-            }
-
-            const deleteParams = {
-              QueueUrl: this.queueUrl,
-              ReceiptHandle: data.Messages[0].ReceiptHandle,
-            };
-            this.sqs.deleteMessage(deleteParams, function (err) {
-              if (err) {
-                console.log('SQS - error delete message', err);
-              } else {
-                console.log('SQS - success delete message', data);
-              }
-            });
-          }
-        });
+        if (command) {
+          sendCommand(command);
+        } else {
+          sendMessage({
+            message: message.Body,
+            userMetadata: metadata,
+          });
+        }
       }
     });
+    
+    app.on('error', (err) => {
+      console.log('SQS - error message', err);
+    });
+    
+    app.on('processing_error', (err) => {
+      console.log('SQS - error processing message', err);
+    });
+    
+    app.start();
   }
 };
